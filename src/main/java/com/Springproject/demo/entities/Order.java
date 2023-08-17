e8f2fca9b96994a4511b4585d9bdebab63a55f56
Jonas Silva <jonasmendocasilva@gmail.com>
2023-08-17 15:49:51 -0400

    Payment entity OneToOne association

--- src/main/java/com/Springproject/demo/entities/Order.java
+++ src/main/java/com/Springproject/demo/entities/Order.java
@@ -8,6 +8,7 @@ import java.util.Set;
 
 import com.Springproject.demo.entities.enm.OrderStatus;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -15,6 +16,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -38,6 +40,9 @@ public class Order implements Serializable{
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
 	
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+	private Payment payment;
+	
 	public Order () {
 	}
 	
@@ -58,7 +63,7 @@ public class Order implements Serializable{
 			this.orderStatus = orderStatus.getCode();
 		}
 	}
-
+	
 	public Long getId() {
 		return id;
 	}
@@ -87,6 +92,14 @@ public class Order implements Serializable{
 		return items;
 	}
 	
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);

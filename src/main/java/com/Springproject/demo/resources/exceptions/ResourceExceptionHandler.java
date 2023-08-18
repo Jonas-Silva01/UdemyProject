25ecc0f7698e29ee54a98fa66db1537a0571c5b8
Jonas Silva <jonasmendocasilva@gmail.com>
2023-08-18 10:10:33 -0400

    Update Exception with 404 error and Json error with 400 error

--- src/main/java/com/Springproject/demo/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/Springproject/demo/resources/exceptions/ResourceExceptionHandler.java
@@ -4,6 +4,7 @@ import java.time.Instant;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -29,5 +30,13 @@ public class ResourceExceptionHandler {
 		HttpStatus status = HttpStatus.BAD_REQUEST;
 		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
+	}
+
+	@ExceptionHandler(HttpMessageNotReadableException.class)
+ 	public ResponseEntity<StandardError> jsonError(RuntimeException e, HttpServletRequest request){
+ 		String error = "JSON error.";
+ 	HttpStatus status = HttpStatus.BAD_REQUEST;
+ 		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(),request.getRequestURI());
+ 		return ResponseEntity.status(status).body(err);
 	}
 }
